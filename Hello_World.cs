a1a6027491b1fac66715a7e129bffdefa0a82fdf
nongsang <shdtkd37@gmail.com>
2018-01-05 03:30:54 +0900

    1월 5일 Hello World!

---
+++ Hello_World.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPPP      // 솔루션 이름을 처음 지을 때 정해진다. 수정해도 상관 없음
+{
+    class Hello_World       // 이 것도 수정해도 상관 없다.
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+// 책은 '시작하세요! C# 7.1 프로그래밍'으로 한다.
