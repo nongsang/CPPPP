022eabfc05d3ff9f2a4944c020c1eba6234bf6f2
nongsang <shdtkd37@gmail.com>
2018-01-09 02:38:16 +0900

    1월 9일 Hello World!

--- 01.Hello_World.cs
+++ 01.Hello_World.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CPPP      // 솔루션 이름을 처음 지을 때 정해진다. 수정해도 상관 없음
+namespace CPPPP      // 솔루션 이름을 처음 지을 때 정해진다. 수정해도 상관 없음
 {
     class Hello_World       // 이 것도 수정해도 상관 없다.
     {
