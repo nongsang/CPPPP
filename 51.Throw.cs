4b9f2661a1e37747a187672c7bec85aea48fe720
nongsang <shdtkd37@gmail.com>
2018-07-24 22:33:55 +0900

    7월 24일 날짜

---
+++ 51.Throw.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPPPP
+{
+    class Throw
+    {
+        private static void HasProblem()
+        {
+            string txt = null;
+
+            Console.WriteLine(txt.ToUpper());
+        }
+
+        public static void Main()
+        {
+            string txt = Console.ReadLine();
+
+            if( txt != "123")
+            {
+                ApplicationException ex = new ApplicationException("틀린 암호");    // 예외용 객체를 생성해
+                throw ex;                                                           // 예외를 던진다.
+            }
+
+            try
+            {
+                HasProblem();           // 일부러 예외를 만들어보자
+            }
+            //catch (System.Exception ex)   // 인스턴스를 던지면
+            //{
+            //    throw ex;                 // 실행되다 예외가 발생한 위치만 알려주고
+            //}
+            catch (System.Exception)        // 객체를 던지면
+            {
+                throw;                      // 실행되다 예외가 발생한 위치와 실제 예외가 발생한 위치까지 알려준다.
+            }                               // 예외를 발생할거면 이 방법을 사용하자
+        }
+    }
+}
---
+++ 52.Date.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPPPP
+{
+    class Date
+    {
+        public static void Main()
+        {
+            DateTime now = DateTime.Now;    // 현재 지역의 현재 시간을 측정한다.
+            Console.WriteLine(now);
+
+            DateTime ChildrenDay = new DateTime(DateTime.Now.Year, 5, 5);   // 시간을 측정하는 시간의 년도와, 월, 일을 정해서 저장할 수 있다.
+            Console.WriteLine(ChildrenDay);
+
+            DateTime today = DateTime.Now;  // 값형식이기 때문에 new로 안넣어도 된다.
+            Console.WriteLine(today + " : " + today.Kind);  // Local이므로 지역 시간대를 반영
+
+            DateTime utcToday = DateTime.UtcNow;    // 협정 세계시를 기준으로 시간을 측정
+            Console.WriteLine(utcToday + " : " + utcToday.Kind);    // Utc이므로 협정 세계시로 시간을 반영
+
+            DateTime worldCup2002 = new DateTime(2002, 5, 31);      // 일일히 값을 넣으려면 new로 해야한다.
+            Console.WriteLine(worldCup2002 + " : " + worldCup2002.Kind);    // 그냥 날자를 쓴것이므로 시간 형식이 정해지지 않았다.
+
+            worldCup2002 = new DateTime(2002, 5, 31, 0, 0, 0, DateTimeKind.Local);  // 시간 형식을 Local로 정의를 한다.
+            Console.WriteLine(worldCup2002 + " : " + worldCup2002.Kind);
+        }
+    }
+}
