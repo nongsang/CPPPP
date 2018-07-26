fa8e746e8cde9d45e4fcae1748bf2aa139e247fc
nongsang <shdtkd37@gmail.com>
2018-07-26 18:27:05 +0900

    7월 26일 익명 메소드와 스레드 맛보기

---
+++ 66.Anonymous_Method.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;         // 스레드를 사용하려면 추가해야 한다.
+
+namespace CPPPP
+{
+    class Anonymous_Method
+    {
+        static void Print()
+        {
+            Console.WriteLine("바보");
+        }
+
+        public static void Main()
+        {
+            //Thread th = new Thread();     // 그냥 인스턴스를 생성할 수 없고 생성과 동시에 실행할 메소드를 전달해야 한다.
+            Thread th1 = new Thread(Print);  // 생성하면서 실행할 메소드의 이름을 전달해준다.
+
+            th1.Start();        // th1 스레드 시작
+
+            Thread th2 = new Thread(
+                delegate (/*매개변수*/)           // 정말 죽어도 1번만 쓸 기능이라면 delegate로 익명메소드를 사용할 수 있다.
+                {
+                    Console.WriteLine("바보");    // 나중에 람다에서 더 자세히 설명한다.
+                });
+
+            th2.Start();        // th2 스레드 시작
+
+            th1.Join();         // th1과 th2에게 Join으로 나머지 스레드들이 전부 작업을 완료할 때까지 기다리라고 명령한다.
+            th2.Join();         // 그렇지 않으면 Main()을 실행하는 스레드가 먼저 끝나는 경우 오류가 발생할 수 있다.
+                                // 스레드는 따로 설명한다.
+        }
+    }
+}
