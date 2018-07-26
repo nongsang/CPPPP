6e3b3a02fb1fbb9e717407eb7bba4eae9c2c51b0
nongsang <shdtkd37@gmail.com>
2018-07-26 22:25:45 +0900

    7월 26일 람다 식과 델리게이트의 합성

---
+++ 73.Lamda_Expression_Delegate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;         // 스레드를 사용하려면 추가해야 한다.
+
+namespace CPPPP
+{
+    class Lamda_Expression_Delegate
+    {
+        delegate int? Divide(int a, int b);     // null을 받을 수 있는 int형 델리게이트
+
+        public static void Main()
+        {
+            Divide div = (a, b) =>      // 람다 식을 델리게이트에 담을 수 있다.
+            {
+                if (b == 0)
+                    return null;
+                else
+                    return a / b;
+            };
+
+            Console.WriteLine("10 / 2 == " + div(10, 2));   // div라는 델리게이트로 람다 식을 대신하여 계산하게 한다.
+            Console.WriteLine("10 / 0 == " + div(10, 0));
+        }
+    }
+}
