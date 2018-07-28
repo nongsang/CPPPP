fbfe61d7a5cf04f2142170c15ce993592ebba0cf
nongsang <shdtkd37@gmail.com>
2018-07-29 05:03:10 +0900

    7월 29일 when 패턴 매칭 조건

---
+++ 95.When_Pattern_Matching.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;    // WriteLine()이라고 줄여서 쓰고싶어
+
+namespace CPPPP
+{
+    class When_Pattern_Matching
+    {
+        public static void Main()
+        {
+            int i = 345;
+
+            if (i > 300) { }    // 이 if문을
+            else { }            // switch문으로 바꿀  수 있다.    
+
+            switch(i)
+            {
+                case int j when j > 300:    // 새롭게 생성한 int형 j에 i값을 저장 후 300을 초과하는지 검사한다.
+                    WriteLine(j);           // 그리고 출력
+                    break;
+
+                default:                    // 그렇지 않다면
+                    WriteLine("바보");      // 출력 안함
+                    break;
+            }
+        }
+    }
+}
