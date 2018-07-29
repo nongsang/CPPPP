d581f7f1688c91a0d2c2a6d4f917a4d4ab55a81b
nongsang <shdtkd37@gmail.com>
2018-07-30 01:43:47 +0900

    7월 29일 람다 식과 델리게이트의 합성

--- 73.Lamda_Expression_Delegate.cs
+++ 73.Lamda_Expression_Delegate.cs
@@ -13,7 +13,7 @@ namespace CPPPP
         public static void Main()
         {
             Divide div = (a, b) =>      // 람다 식을 델리게이트에 담을 수 있다.
-            {
+            {                           // 그리고 매개변수가 a, b 두개 이므로 (a, b)라고 묶어야 한다.
                 if (b == 0)
                     return null;
                 else
@@ -21,7 +21,7 @@ namespace CPPPP
             };
 
             Console.WriteLine("10 / 2 == " + div(10, 2));   // div라는 델리게이트로 람다 식을 대신하여 계산하게 한다.
-            Console.WriteLine("10 / 0 == " + div(10, 0));
+            Console.WriteLine("10 / 0 == " + div(10, 0));   // 두번째 인수가 0이므로 null 반환
         }
     }
 }
