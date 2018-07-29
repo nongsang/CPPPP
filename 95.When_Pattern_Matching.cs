eb80f5253f3e75d92e83af8615a44978484e5775
nongsang <shdtkd37@gmail.com>
2018-07-29 16:49:34 +0900

    7월 29일 when 패턴 매칭 조건과 주의점

--- 95.When_Pattern_Matching.cs
+++ 95.When_Pattern_Matching.cs
@@ -25,12 +25,12 @@ namespace CPPPP
 
             switch (txt)
             {
-                case var item when (ContainsAt(item, "http://www.naver.com")):  // 'http://'까지 풀네임으로 써야 제대로 출력된다.
+                case var item when (ContainsAt(item, "http://www.naver.com")):  // when을 사용하여 var 패턴 매칭의 활용도를 높여주었다.
                     WriteLine("In Naver");
                     break;
 
-                case var item when (ContainsAt(item, "http://www.daum.net")):   // 그냥 www.*만 써주면 예외를 날린다.
-                    WriteLine("In Daum");
+                case var item when (ContainsAt(item, "http://www.daum.net")):   // 주의할 점은'http://'까지 풀네임으로 써야 제대로 출력된다.
+                    WriteLine("In Daum");                                       // 그냥 www.*만 써주면 예외를 날린다.
                     break;
 
                 default:
@@ -50,6 +50,25 @@ namespace CPPPP
                     WriteLine("바보");
                     break;
             }
+
+            Action<(int, int)> detectZeroOR = (arg) =>      // 주의해야할 점은 Action<int, int>이 아니라 Action<(int, int)>이다.
+            {                                               // 람다 전용 델리게이트 Action<>과 튜플을 섞어서 람다로 실행한다.
+                switch (arg)                                // 튜플을 전달하고 있음에 주의해야 한다.
+                {
+                    case var r when r.Equals((0, 0)):       // r이 (0, 0)값을 가진 튜플과 같거나
+                    case var r1 when r1.Item1 == 0:         // 새롭게 만든 튜플 r1의 첫번째 원소가 0이거나
+                    case var r2 when r2.Item2 == 0:         // 새롭게 만든 튜플 r2의 두번째 원소가 0이면
+                        Console.WriteLine("Zero found.");   // 0을 찾았다는 말과 함께
+                        return;                             // 람다 종료
+                }
+
+                Console.WriteLine("Both nonzero.");     // 모두 아니면 0을 못찾았다는 말 출력.
+            };
+
+            detectZeroOR((0, 0));           // 이것도 0, 0이 아니라 (0, 0)을 전달한다.
+            detectZeroOR((1, 0));           // ()로 묶어줘야
+            detectZeroOR((0, 10));          // 튜플로 전달한다.
+            detectZeroOR((10, 15));         // 반드시 주의해서 보자
         }
     }
 }
