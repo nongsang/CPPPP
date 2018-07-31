f52b9c2c32bdcdea6e04442fe607496a155c6d4b
nongsang <shdtkd37@gmail.com>
2018-07-31 17:26:56 +0900

    7월 31일 단축 평가

---
+++ 005.Short_Circuit_Evaluation.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CPPPP
+{
+    class Short_Circuit_Evaluation
+    {
+        public static void Main()
+        {
+            int n1 = 10;
+            int n2 = 6;
+
+            bool result = ((n1 % 3) == 0 && (n2 % 3) == 0); // (n1 % 3) == 0에서 이미 false이기 때문에 (n2 % 3) == 0는 연산을 하지 않는다.
+                                                            // 이를 단락 계산 또는 단축 평가라고 한다.
+        }
+    }
+}
