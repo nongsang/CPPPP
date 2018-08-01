ddc92c64ff2c62cb84a967077354aa15da641178
nongsang <shdtkd37@gmail.com>
2018-08-01 12:48:41 +0900

    8월 1일 날짜

--- 061.Date_Time.cs
+++ 061.Date_Time.cs
@@ -18,6 +18,7 @@ namespace CPPPP
 
             DateTime today = DateTime.Now;  // 값형식이기 때문에 new로 안넣어도 된다.
             Console.WriteLine(today + " : " + today.Kind);  // Local이므로 지역 시간대를 반영
+                                                            // kind로 시간의 형식 알 수 있다.
 
             DateTime utcToday = DateTime.UtcNow;    // 협정 세계시를 기준으로 시간을 측정
             Console.WriteLine(utcToday + " : " + utcToday.Kind);    // Utc이므로 협정 세계시로 시간을 반영
