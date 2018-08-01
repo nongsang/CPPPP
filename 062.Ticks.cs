dd209adcc926837c063ed37f4fa5563f1e8d1063
nongsang <shdtkd37@gmail.com>
2018-08-01 15:09:17 +0900

    8월 1일 시간측정

--- 062.Ticks.cs
+++ 062.Ticks.cs
@@ -25,6 +25,8 @@ namespace CPPPP
             DateTime after = DateTime.Now;      // 알고리즘을 돌린 후 날짜를 기록한다.
 
             long gap = after.Ticks - before.Ticks;      // 그리고 Ticks라는 100ns 정밀도를 가진 속성으로 시간차를 측정한다.
+                                                        // Stopwatch에서 알려주는 Ticks와는 다르다.
+                                                        // Stopwatch의 Ticks는 타이머 틱 수를 반환하기 때문에 컴퓨터마다 값이 다를 수 있다.
 
             Console.WriteLine("Total Ticks : " + gap);  // 몇 틱이 흘렀는가 계산
             Console.WriteLine("Millisecond : " + (gap / 10000));    // 100ns가 기준이므로 10000으로 나누면 ms를 반환
