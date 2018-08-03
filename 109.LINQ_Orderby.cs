39cc562abc8f15dce063195eb82b70d5d2a12f01
nongsang <shdtkd37@gmail.com>
2018-08-03 18:01:16 +0900

    8월 3일 LINQ orderby(정렬)

--- 109.LINQ_Orderby.cs
+++ 109.LINQ_Orderby.cs
@@ -33,11 +33,12 @@ namespace CPPPP
                 new Person { Name = "Nietzsche", Age = 23, Address = "Germany" },
             };
 
-            var ageOver30 = from person in people       // person이란 레퍼런스로 people 집단 중에서 원소 하나씩 지정하는데
-                            orderby person.Age ascending// person이 가리키는 원소의 Age의 오름차순으로 정렬, ascending이 없어도 자동 오름차순
-                                                        // 내림차순으로 하고 싶으면 decending으로 바꾼다.
-                            where person.Age > 30       // 원소의 Age 필드의 값이 30 초과인 원소의
-                            select person;              // 원소 전체를 반환
+            var ageOver30 = from person in people           // person이란 레퍼런스로 people 집단 중에서 원소 하나씩 지정하는데
+                            orderby person.Age ascending    // person이 가리키는 원소의 Age의 오름차순으로 정렬
+                            orderby person.Age              // ascending이 없어도 자동 오름차순
+                            //orderby person.Age descending // 내림차순으로 하고 싶으면 decending으로 바꾼다.
+                            where person.Age > 30           // 원소의 Age 필드의 값이 30 초과인 원소의
+                            select person;                  // 원소 전체를 반환
 
             foreach (var item in ageOver30)     // 전체
                 Console.WriteLine(item);        // 출력
