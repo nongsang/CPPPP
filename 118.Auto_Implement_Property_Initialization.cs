69c82ce6a34e27b70294ce7430e05581fcdd13bb
nongsang <shdtkd37@gmail.com>
2018-08-04 00:37:30 +0900

    8월 4일 자동 구현 속성 초기화

---
+++ 118.Auto_Implement_Property_Initialization.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Person
+{
+    //public string Name { get; set; }            // 예전에는 자동구현 속성인 필드의 초기값을 가지고 싶으면
+
+    //public Person()                             // 생성자를 사용하여
+    //{
+    //    this.Name = "바보";                     // 초기화를 해줬어야 했다.
+    //}
+
+    public string Name { get; set; } = "바보";    // C# 6.0부터 자동구현 속성에 초기화가 가능해졌다.
+}
+
+namespace CPPPP
+{
+    class Auto_Implement_Property_Initialization
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
