c1d550107af559854c569c60609527fe1350187a
nongsang <shdtkd37@gmail.com>
2018-08-21 04:05:22 +0900

    8월 21일 정규표현식

--- 070.Regular_Expression.cs
+++ 070.Regular_Expression.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Text.RegularExpressions;   // 정규표현식을 그냥 사용하려면 추가해야 한다.
+using System.Text.RegularExpressions;   // 정규표현식을 사용하려면 추가해야 한다.
 
 namespace CPPPP
 {
@@ -12,19 +12,22 @@ namespace CPPPP
         static bool IsEmail(string email)
         {
             // 정규표현식
-            // C#에서는 제일 앞에 @를 써야 정규표현식을 사용한다는 의미다.
+            // @는 축자 식별자(Verbatim Identifier)로 C# 6.0부터 사용할 수 있는 기능이다.
+            // 자세한 내용은 071.Verbatim_Identifier에서 다룬다.
             // ^ : 정규표현식 시작
-            // 0-9 : 0 ~ 9
-            // a-zA-Z : 대문자, 소문자
-            // [] : 문자클래스, 배열같은것
-            // @ : 반드시 들어가야하는 문자
+            // () : 그룹을 나타낸다.
+            // 0-9a-zA-Z : 숫자 0 ~ 9, 소문자 a ~ z, 대문자 A ~ Z 모두를 포함한다.
+            // [] : 문자클래스, 배열같은 것
             // + : 앞의 문자가 1개 이상
+            // @ : 반드시 포함되는 리터럴 문자열
             // \. : 정규표현식에서 .은 일종의 문자를 의미한다.
             //      \가 문자열에서 이스케이프시퀀스를 의미하므로 \문자를 사용하려면 \\를 사용해야 한다.
-            //      정규표현식도 마찬가지로 .을 문자열로 쓰려면 \.를 넣어야 한다.
+            //      .을 문자열로 출력하려면 C처럼 \.로 넣어야 한다.
+            //      $를 빼면 .는 순수하게 정규표현식으로 인식하므로 \를 넣지 않아도 된다.
             // {n,} : 앞 문자가 n번 이상 반복
             // & : 정규표현식의 끝을 알림
-            Regex regex = new Regex(@"^([0-9a-zA-Z]+)@([0-9a-zA-Z]+)(\.[0-9a-zA-Z]+){1,}$");    // 정규표현식 정의
+            //Regex regex = new Regex(@"^([0-9a-zA-Z]+)@([0-9a-zA-Z]+)(\.[0-9a-zA-Z]+){1,}$");      // 정규표현식 정의
+            Regex regex = new Regex("^([0-9a-zA-Z]+)@([0-9a-zA-Z]+)(.[0-9a-zA-Z]+){1,}$");          // @를 빼면 \.에서 .로 바꿔도 된다.  
             return regex.IsMatch(email);
         }
 
@@ -38,9 +41,11 @@ namespace CPPPP
 
             string txt = "Hello World! Welcome to my world";         // World는 앞글자가 대문자다.
 
-            Regex regex = new Regex("world", RegexOptions.IgnoreCase);  // world라는 단어와 들어온 문자열의 단어가 다르면 체크한다.
-                                                                        // World와 world는 패턴이 일치하지만 다른 단어로 인식한다.
-            string result = regex.Replace(txt, "Universe!");    // Replace는 다른 단어라고 체크된 문자를 새로문 문자열로 대체하는 메소드다.
+            Regex regex = new Regex("world", RegexOptions.IgnoreCase);  // world라는 단어와 같으면 체크하는 기능이다.
+                                                                        // World와 world는 대소문자가 다르기 때문에 다른 단어로 인식한다.
+                                                                        // 하지만 패턴이 일치하므로 World와 world를 체크한다.
+            string result = regex.Replace(txt, "Universe!");    // Replace는 체크된 문자를 새로운 문자열로 대체하는 메소드다.
+                                                                // regex에 저장된 단어와 txt 문자열을 비교하여 Universe!라는 단어로 대체한다
                                                                 // 따라서 World와 world를 Universe로 대체하게 된다.
             Console.WriteLine(result);
         }
